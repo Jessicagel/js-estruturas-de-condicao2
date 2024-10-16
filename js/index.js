aa4c9d57cb4db2e04d6323e79cb8b24fbd08c38d
jessicagel <jessicaafisio@gmail.com>
2024-10-16 08:35:12 -0300

    alteraçoes

---
+++ js/index.js
@@ -0,0 +1,49 @@
+function (event){
+    event.preventDefault();
+
+    let meuLabel = document.getElementById("dia");
+    // let meuDisplay = document.getElementById("display");
+    let meuDia = meuInput.value;
+
+    meuDia = parseFloat(meuDia);
+
+let meuDia = Number(prompt('Digite seu dia'));
+
+if (ultimoDigito [1,2]){
+nome = 'Desenvolvedor (a)'
+}else if (ultimoDigito [3, 4, 5]){
+nome = 'Programador(a)'
+}else if (ultimoDigito [6, 7, 8]){
+nome = 'Estagiário(a)'
+}else (ultimoDigito [9,0]);{
+nome = 'Senior(a)'
+}
+
+let meuLabel = document.getElementById("mes");
+let meuMes = meuInput.value;
+
+if (meuMes === janeiro){
+    nome += 'bugado(a)'
+    }else if (meuMes === 'fevereiro'){
+    nome += 'do CTRL C, CTROL V'
+    }else if (meuMes === 'março'){
+    nome += 'das gambiarras'
+    }else if (meuMes === 'abril'){
+    nome += 'Que culpa os cache'
+    }else if (meuMes === 'maio'){
+    nome += 'que esquece o que faz'
+    }else if (meuMes === 'junho'){
+    nome += 'do git vazio'
+    }else if (meuMes === 'julho'){
+    nome += 'das try/catch vazia'
+    }else if (meuMes === 'agosto'){
+    nome += 'famosinho do linkedin'
+    }else if (meuMes === 'setembro'){
+    nome += 'caçador de bugs'
+    }else if (meuMes === 'outubro'){
+    nome += 'do windows pirata'
+    }else if (meuMes === 'novembro'){
+    nome += 'do update sem where'
+    }else (meuMes === 'dezembro');{
+    nome += 'do commit bugado'
+    }}
